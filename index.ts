889137c8e0c504c5e4fd2b27ca4e29763f6662a4
Baali Boudjemaa <baaliboudjemaaens@gmail.com>
2026-04-10 16:01:17 -0700

    add electron index

---
+++ index.ts
@@ -0,0 +1,54 @@
+import { app, BrowserWindow, } from "electron";
+import { join } from "node:path";
+import { prepareNext } from "sc-prepare-next";
+import { bootstrap } from './src/main';
+ const PORT = 4444;
+
+async function createWindow(): Promise<void> {
+  const win = new BrowserWindow({
+    title: "SC - Electron and Next",
+    icon: "./build/icon.png",
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: join(__dirname, "preload.js"),
+    },
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(join(__dirname, "..", "..", "dist", "frontend", "index.html"));
+  } else {
+    await bootstrap();
+    win.loadURL(`http://localhost:${PORT}/`);
+    win.webContents.openDevTools();
+  }
+}
+
+/**
+ * When the application is ready, this function is called.
+ *
+ * It creates a BrowserWindow instance and loads the main application.
+ * It also sets up the logging and database connections.
+ *
+ * @returns {Promise<void>} A Promise that resolves when all the setup is done.
+ */
+app.whenReady().then(async () => {
+  await prepareNext("./src", PORT);
+
+
+
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+/* ++++++++++ events ++++++++++ */
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+/* ++++++++++ code ++++++++++ */
