36cae645cf66a70da652ff78cd5d978146788874
Baali Boudjemaa <baaliboudjemaaens@gmail.com>
2026-05-18 12:28:18 -0700

    made changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
-import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import { AppModule } from './app.module';
 console.log(process.env.IS_NEXT);
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4444',
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4444',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -17,5 +17,6 @@ export async function bootstrap() {
   await app.listen(process.env.PORT ?? 3002);
 }
 if (process.env.IS_NEXT === 'NO') {
-  bootstrap();
+
 }
+bootstrap();
